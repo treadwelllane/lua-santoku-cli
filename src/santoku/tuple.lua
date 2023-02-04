63f99ec1130607865f67a07863ed6bf301ce20b5
broma0 <121064585+broma0@users.noreply.github.com>
2023-02-04 14:38:28 -0500

    simplify tuple implementation

--- src/santoku/tuple.lua
+++ src/santoku/tuple.lua
@@ -1,26 +1,14 @@
-local arglist = function (n)
-  local args = {}
-  for i = 1, n do
-    args[i] = "arg" .. i
+local function tuple (n, a, ...)
+  if n == 0 then
+    return function (...) return ... end
+  else 
+    local rest = tuple(n - 1, ...)
+    return function (...)
+      return a, rest(...)
+    end
   end
-  return table.concat(args, ", ")
 end
 
-local tuple = {}
-
 return function (...)
-  local m = select("#", ...)
-  if m == 0 then
-    return function (...) return ... end, 0
-  elseif not tuple[m] then
-    local args = arglist(m)
-    tuple[m] = loadstring(table.concat({[[
-      return function (]], args, [[)
-        return function (...)
-          return select(n or 1, ]], args, [[, ...)
-        end, ]], m, [[
-      end
-    ]]}))()
-  end
-  return tuple[m](...)
+  return tuple(select("#", ...), ...)
 end
