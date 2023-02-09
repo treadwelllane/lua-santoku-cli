333a54379e8062c1e5416dfadf22cf3c3902bb11
broma0 <121064585+broma0@users.noreply.github.com>
2023-02-09 08:56:55 -0500

    refactor gen module and remove gensent

--- src/santoku/tuple.lua
+++ src/santoku/tuple.lua
@@ -1,3 +1,5 @@
+local M = {}
+
 local function tuple (n, a, ...)
   if n == 0 then
     return function (...) 
@@ -11,6 +13,38 @@ local function tuple (n, a, ...)
   end
 end
 
-return function (...)
+M.equals = function (a, ...)
+  local m = select("#", a())
+  local ts = select("#", ...)
+  for j = 1, ts do
+    local b = select(j, ...)
+    if b == nil then
+      return false
+    end
+    local n = select("#", b()) 
+    if m ~= n then
+      return false
+    end
+  end
+  for i = 1, m do
+    local v = select(i, a())
+    for j = 1, ts do
+      local b = select(j, ...)
+      local w = select(i, b()) 
+      if v ~= w then
+        return false
+      end
+    end
+  end
+  return true
+end
+
+M.tuple = function (...)
   return tuple(select("#", ...), ...)
 end
+
+return setmetatable(M, {
+  __call = function (_, ...)
+    return M.tuple(...)
+  end
+})
