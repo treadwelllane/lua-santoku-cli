9fc017fe0000230e791b8d3358aabafc9f2b0efc
broma0 <121064585+broma0@users.noreply.github.com>
2023-05-06 16:29:32 +0000

    update phony targets and whitespace

--- config/lib.mk
+++ config/lib.mk
@@ -1,22 +1,19 @@
 LIB_NAME = $(NAME)
 
-LIB_ROCKSPEC = $(BUILD_DIR)/$(LIB_NAME)-$(LIB_VERSION).rockspec
-LIB_ROCKSPEC_T = config/lib.rockspec
-
 LIB_SRC = $(shell find src/$(LIB_NAME) -name '*.lua')
 LIB_DIST = $(patsubst src/%, $(INST_LUADIR)/%, $(LIB_SRC))
 
 LIB_ROCKSPEC = $(BUILD_DIR)/$(LIB_NAME)-$(VERSION).rockspec
 LIB_ROCKSPEC_T = config/lib.rockspec
 
-test: $(LIB_ROCKSPEC) 
+test: $(LIB_ROCKSPEC)
 	luarocks test $(LIB_ROCKSPEC) $(ARGS)
 
-iterate: $(LIB_ROCKSPEC) 
+iterate: $(LIB_ROCKSPEC)
 	luarocks test $(LIB_ROCKSPEC) iterate $(ARGS)
 
 lib-install: $(LIB_ROCKSPEC)
-	luarocks make $(LIB_ROCKSPEC) $(ARGS) 
+	luarocks make $(LIB_ROCKSPEC) $(ARGS)
 
 luarocks-lib-install: $(LIB_DIST)
 
@@ -24,7 +21,7 @@ lib-upload: $(LIB_ROCKSPEC)
 	cd "$(BUILD_DIR)" && \
 		luarocks upload --api-key "$(LUAROCKS_API_KEY)" "../$(LIB_ROCKSPEC)" $(ARGS)
 
-$(INST_LUADIR)/$(LIB_NAME)/%: src/$(LIB_NAME)/% 
+$(INST_LUADIR)/$(LIB_NAME)/%: src/$(LIB_NAME)/%
 	test -n "$(INST_LUADIR)"
 	mkdir -p "$(dir $@)"
 	cp "$^" "$@"
@@ -35,4 +32,4 @@ $(LIB_ROCKSPEC): $(LIB_ROCKSPEC_T)
 	GIT_URL="$(GIT_URL)" \
 		$(TOKU) template -f "$^" -o "$@"
 
-.PHONY: test lib-install luarocks-lib-install lib-upload
+.PHONY: test iterate lib-install luarocks-lib-install lib-upload
