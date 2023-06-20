cded5441cc11186d036165a577511aa18b499e90
broma0 <121064585+broma0@users.noreply.github.com>
2023-06-20 23:37:19 +0000

    make makefile variables optional

--- config/cli.mk
+++ config/cli.mk
@@ -1,12 +1,12 @@
-CLI_NAME = $(NAME)-cli
+CLI_NAME ?= $(NAME)-cli
 
-CLI_ROCKSPEC = $(BUILD_DIR)/$(CLI_NAME)-$(VERSION).rockspec
-CLI_ROCKSPEC_T = config/cli.rockspec
+CLI_ROCKSPEC ?= $(BUILD_DIR)/$(CLI_NAME)-$(VERSION).rockspec
+CLI_ROCKSPEC_T ?= config/cli.rockspec
 
-CLI_SRC = src/santoku-cli.lua
-CLI_DEST = $(INST_BINDIR)/toku
+CLI_SRC ?= src/santoku-cli.lua
+CLI_DEST ?= $(INST_BINDIR)/toku
 
-TOKU = LUA_PATH="src/?.lua;$(LUA_PATH)" $(LUA) $(CLI_SRC)
+TOKU ?= LUA_PATH="src/?.lua;$(LUA_PATH)" $(LUA) $(CLI_SRC)
 
 cli-install: $(CLI_ROCKSPEC)
 	luarocks make $(CLI_ROCKSPEC) $(ARGS)
