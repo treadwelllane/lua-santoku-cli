e4d123f4617d15b45eefa9380713ce7ed65ade23
Matthew Brooks <mb964033@gmail.com>
2023-10-30 11:54:17 -0400

    remove santoku libs (this project will just be cli)

--- config.lua
+++ config.lua
@@ -1,8 +1,8 @@
 local _ENV = {}
 
-name = "santoku"
+name = "santoku-cli"
 version = "0.0.94-1"
-variable_prefix = "TK"
+variable_prefix = "TK_CLI"
 
 license = "MIT"
 
@@ -10,34 +10,17 @@ luacov_include = {
   "^%./santoku.*"
 }
 
--- TODO: can we do optional dependencies for
--- things like luafilesystem, socket, sqlite,
--- posix, etc?
---
--- TODO: Create a santoku lib to gracefully wrap
--- functions which require an optional
--- dependency
 dependencies = {
-
   "lua >= 5.1",
-
-  -- Optional dependencies:
-
-  -- "lua-zlib >= 1.2-2",
-  -- "luafilesystem >= 1.8.0-1",
-  -- "lsqlite3 >= 0.9.5",
-  -- "inspect >= 3.1.3-0"
-
+  "santoku >= 0.0.97-1",
+  "luafilesystem >= 1.8.0-1",
 }
 
 test_dependencies = {
   "luacov >= 0.15.0",
   "luacheck >= 1.1.0-1",
-  "lua-zlib >= 1.2-2",
-  "luafilesystem >= 1.8.0-1",
-  "lsqlite3 >= 0.9.5",
   "inspect >= 3.1.3-0",
-  "luassert >= 1.9.0-1"
+  "luassert >= 1.9.0-1",
 }
 
 homepage = "https://github.com/treadwelllane/lua-" .. name
@@ -47,8 +30,6 @@ download = homepage .. "/releases/download/" .. version .. "/" .. tarball
 return {
   env = _ENV,
   excludes = {
-    "src/santoku/template.lua",
-    "test/spec/santoku/template.lua",
     "test/spec/santoku/cli/template.lua"
   },
 }
