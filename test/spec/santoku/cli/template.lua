90a00770b8077e0549c0940bd96293a36aeaabf5
broma0 <121064585+broma0@users.noreply.github.com>
2023-10-31 12:00:43 +0000

    add release automation and public flag

--- test/spec/santoku/cli/template.lua
+++ test/spec/santoku/cli/template.lua
@@ -7,7 +7,7 @@ test("santoku-cli", function ()
   test("template", function ()
 
     test("should allow stdin/stdout processing", function ()
-      local toku = os.getenv("LUA") .. " bin/toku.lua"
+      local toku = os.getenv("LUA") .. " -l luacov bin/toku.lua"
       local ok, gen = sys.sh("echo '<% return \"hello\" %>' | ", toku, " template -f - -o -")
       assert.equals(true, ok, gen)
       assert.equals("hello", gen:co():head())
