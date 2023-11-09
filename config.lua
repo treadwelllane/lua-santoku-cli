30d8b20a97b3dfb01f273fa89798161e4a064109
broma0 <121064585+broma0@users.noreply.github.com>
2023-11-09 19:49:42 +0000

    refactor makefile structure and layout

--- config.lua
+++ config.lua
@@ -1,31 +1,34 @@
-local _ENV = {}
+local env = {
 
-name = "santoku-cli"
-version = "0.0.106-1"
-variable_prefix = "TK_CLI"
-license = "MIT"
-public = true
+  name = "santoku-cli",
+  version = "0.0.107-1",
+  variable_prefix = "TK_CLI",
+  license = "MIT",
+  public = true,
 
-dependencies = {
-  "lua >= 5.1",
-  "santoku >= 0.0.113-1",
-  "luafilesystem >= 1.8.0-1",
-  "argparse >= 0.7.1-1",
+  dependencies = {
+    "lua >= 5.1",
+    "santoku >= 0.0.118-1",
+    "luafilesystem >= 1.8.0-1",
+    "argparse >= 0.7.1-1",
+  },
+
+  test_dependencies = {
+    "luacov >= 0.15.0-1",
+    "luacheck >= 1.1.0-1",
+    "inspect >= 3.1.3-0",
+    "luassert >= 1.9.0-1",
+    "luaposix >= 36.2.1-1"
+  },
+
 }
 
-test_dependencies = {
-  "luacov >= 0.15.0-1",
-  "luacheck >= 1.1.0-1",
-  "inspect >= 3.1.3-0",
-  "luassert >= 1.9.0-1",
-}
-
-homepage = "https://github.com/treadwelllane/lua-" .. name
-tarball = name .. "-" .. version .. ".tar.gz"
-download = homepage .. "/releases/download/" .. version .. "/" .. tarball
+env.homepage = "https://github.com/treadwelllane/lua-" .. env.name
+env.tarball = env.name .. "-" .. env.version .. ".tar.gz"
+env.download = env.homepage .. "/releases/download/" .. env.version .. "/" .. env.tarball
 
 return {
-  env = _ENV,
+  env = env,
   excludes = {
     "test/spec/santoku/cli/template.lua"
   },
