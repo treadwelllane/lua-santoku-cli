3b331b666c6e81033af2bca1db2d4a22675bf030
Matthew Brooks <mb964033@gmail.com>
2023-11-28 22:49:16 +0000

    bump santoku-cli

--- config.lua
+++ config.lua
@@ -1,14 +1,14 @@
 local env = {
 
   name = "santoku-cli",
-  version = "0.0.112-1",
+  version = "0.0.113-1",
   variable_prefix = "TK_CLI",
   license = "MIT",
   public = true,
 
   dependencies = {
     "lua >= 5.1",
-    "santoku >= 0.0.127-1",
+    "santoku >= 0.0.134-1",
     "luaposix >= 36.2.1-1",
     "argparse >= 0.7.1-1",
   },
