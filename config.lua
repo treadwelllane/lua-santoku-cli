23d0a38fa57bba7a37e1dbd4c889346ede6a8cae
Matthew Brooks <mb964033@gmail.com>
2023-12-16 23:12:33 +0000

    versions

--- config.lua
+++ config.lua
@@ -1,7 +1,7 @@
 local env = {
 
   name = "santoku-cli",
-  version = "0.0.122-1",
+  version = "0.0.123-1",
   variable_prefix = "TK_CLI",
   license = "MIT",
   public = true,
@@ -9,11 +9,11 @@ local env = {
   dependencies = {
     "lua >= 5.1",
     "santoku >= 0.0.147-1",
-    "santoku-fs >= 0.0.3-1",
-    "santoku-template >= 0.0.2-1",
-    "santoku-bundle >= 0.0.6-1",
-    "santoku-system >= 0.0.2-1",
-    "santoku-test-runner >= 0.0.2-1",
+    "santoku-fs >= 0.0.5-1",
+    "santoku-template >= 0.0.3-1",
+    "santoku-bundle >= 0.0.7-1",
+    "santoku-system >= 0.0.3-1",
+    "santoku-test-runner >= 0.0.3-1",
     "argparse >= 0.7.1-1",
   },
 
