2895acbced3bf5ae63573ee9ad22fb76bc0ebf7d
broma0 <121064585+broma0@users.noreply.github.com>
2023-12-22 14:51:09 +0000

    bump version to 0.0.135-1

--- config.lua
+++ config.lua
@@ -1,7 +1,7 @@
 local env = {
 
   name = "santoku-cli",
-  version = "0.0.134-1",
+  version = "0.0.135-1",
   variable_prefix = "TK_CLI",
   license = "MIT",
   public = true,
@@ -13,7 +13,7 @@ local env = {
     "santoku-template >= 0.0.6-1",
     "santoku-bundle >= 0.0.14-1",
     "santoku-system >= 0.0.4-1",
-    "santoku-test-runner >= 0.0.6-1",
+    "santoku-test-runner >= 0.0.7-1",
     "argparse >= 0.7.1-1",
   },
 
