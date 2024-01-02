47e2ae854e850f9930a1b4f9a590050672f2a922
Matthew Brooks <mb964033@gmail.com>
2024-01-02 03:37:21 +0000

    update bundle

--- make.lua
+++ make.lua
@@ -8,11 +8,11 @@ local env = {
 
   dependencies = {
     "lua >= 5.1",
-    "santoku >= 0.0.158-1",
+    "santoku >= 0.0.159-1",
     "santoku-fs >= 0.0.13-1",
     "santoku-template >= 0.0.8-1",
-    "santoku-bundle >= 0.0.16-1",
-    "santoku-system >= 0.0.8-1",
+    "santoku-bundle >= 0.0.17-1",
+    "santoku-system >= 0.0.9-1",
     "santoku-test-runner >= 0.0.10-1",
     "santoku-make >= 0.0.27-1",
     "argparse >= 0.7.1-1",
