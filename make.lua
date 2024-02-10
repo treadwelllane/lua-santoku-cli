19a4209a8f489b038b37e9dcda0af8669c4b23e3
Matthew Brooks <mb964033@gmail.com>
2024-02-10 16:24:42 +0000

    update deps

--- make.lua
+++ make.lua
@@ -1,20 +1,20 @@
 local env = {
 
   name = "santoku-cli",
-  version = "0.0.178-1",
+  version = "0.0.179-1",
   variable_prefix = "TK_CLI",
   license = "MIT",
   public = true,
 
   dependencies = {
     "lua >= 5.1",
-    "santoku >= 0.0.189-1",
-    "santoku-fs >= 0.0.28-1",
+    "santoku >= 0.0.193-1",
+    "santoku-fs >= 0.0.29-1",
     "santoku-template >= 0.0.20-1",
     "santoku-bundle >= 0.0.27-1",
-    "santoku-system >= 0.0.21-1",
+    "santoku-system >= 0.0.22-1",
     "santoku-test-runner >= 0.0.22-1",
-    "santoku-make >= 0.0.40-1",
+    "santoku-make >= 0.0.41-1",
     "argparse >= 0.7.1-1",
   },
 
