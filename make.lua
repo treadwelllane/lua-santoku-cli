bedc5dd2aa4030ea7312e299e3ac925420fe3aad
broma0 <121064585+broma0@users.noreply.github.com>
2024-03-04 14:43:28 +0000

    pin dependency versions

--- make.lua
+++ make.lua
@@ -1,27 +1,27 @@
 local env = {
 
   name = "santoku-cli",
-  version = "0.0.167-1",
+  version = "0.0.167-2",
   variable_prefix = "TK_CLI",
   license = "MIT",
   public = true,
 
   dependencies = {
-    "lua >= 5.1",
-    "santoku >= 0.0.159-1",
-    "santoku-fs >= 0.0.13-1",
-    "santoku-template >= 0.0.10-1",
-    "santoku-bundle >= 0.0.19-1",
-    "santoku-system >= 0.0.11-1",
-    "santoku-test-runner >= 0.0.11-1",
-    "santoku-make >= 0.0.31-1",
-    "argparse >= 0.7.1-1",
+    "lua == 5.1",
+    "santoku == 0.0.159-1",
+    "santoku-fs == 0.0.13-1",
+    "santoku-template == 0.0.10-1",
+    "santoku-bundle == 0.0.19-1",
+    "santoku-system == 0.0.11-1",
+    "santoku-test-runner == 0.0.11-1",
+    "santoku-make == 0.0.31-1",
+    "argparse == 0.7.1-1",
   },
 
   test = {
     dependencies = {
-      "santoku-test >= 0.0.6-1",
-      "luacov >= 0.15.0-1",
+      "santoku-test == 0.0.6-1",
+      "luacov == 0.15.0-1",
     },
   }
 
