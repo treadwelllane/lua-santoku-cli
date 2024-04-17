97ba1768edca54e3d73f500039cce507ccc00947
Matthew Brooks <mb964033@gmail.com>
2024-04-17 15:15:08 +0000

    bump version

--- make.lua
+++ make.lua
@@ -1,7 +1,7 @@
 local env = {
 
   name = "santoku-cli",
-  version = "0.0.188-1",
+  version = "0.0.189-1",
   variable_prefix = "TK_CLI",
   license = "MIT",
   public = true,
@@ -11,8 +11,8 @@ local env = {
     "santoku >= 0.0.203-1",
     "santoku-fs >= 0.0.30-1",
     "santoku-template >= 0.0.22-1",
-    "santoku-bundle >= 0.0.27-1",
-    "santoku-system >= 0.0.22-1",
+    "santoku-bundle >= 0.0.28-1",
+    "santoku-system >= 0.0.23-1",
     "santoku-test-runner >= 0.0.22-1",
     "santoku-make >= 0.0.53-1",
     "argparse >= 0.7.1-1",
