c26b8f67602b01e30145d5b4632a93671b19864c
Matthew Brooks <mb964033@gmail.com>
2024-04-17 15:56:36 +0000

    see last

--- make.lua
+++ make.lua
@@ -14,7 +14,7 @@ local env = {
     "santoku-bundle >= 0.0.28-1",
     "santoku-system >= 0.0.23-1",
     "santoku-test-runner >= 0.0.22-1",
-    "santoku-make >= 0.0.55-1",
+    "santoku-make >= 0.0.54-1",
     "argparse >= 0.7.1-1",
   },
 
