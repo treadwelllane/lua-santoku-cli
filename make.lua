2ec562a743947128bdcd45663324ae6884a220d6
Matthew Brooks <mb964033@gmail.com>
2025-02-28 22:38:25 +0000

    update bundler

--- make.lua
+++ make.lua
@@ -1,7 +1,7 @@
 local env = {
 
   name = "santoku-cli",
-  version = "0.0.251-1",
+  version = "0.0.252-1",
   variable_prefix = "TK_CLI",
   license = "MIT",
   public = true,
@@ -11,7 +11,7 @@ local env = {
     "santoku >= 0.0.245-1",
     "santoku-fs >= 0.0.33-1",
     "santoku-template >= 0.0.26-1",
-    "santoku-bundle >= 0.0.30-1",
+    "santoku-bundle >= 0.0.31-1",
     "santoku-system >= 0.0.33-1",
     "santoku-test-runner >= 0.0.23-1",
     "santoku-make >= 0.0.109-1",
