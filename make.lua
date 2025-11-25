25d69dd76d8bb2993c4a573974c078987c4d1397
broma0 <121064585+broma0@users.noreply.github.com>
2025-11-24 20:10:13 -0500

    add mustache command and refactor cli

--- make.lua
+++ make.lua
@@ -1,20 +1,20 @@
 local env = {
 
   name = "santoku-cli",
-  version = "0.0.279-1",
+  version = "0.0.280-1",
   variable_prefix = "TK_CLI",
   license = "MIT",
   public = true,
 
   dependencies = {
     "lua >= 5.1",
-    "santoku >= 0.0.294-1",
+    "santoku >= 0.0.297-1",
     "santoku-fs >= 0.0.34-1",
     "santoku-template >= 0.0.30-1",
     "santoku-bundle >= 0.0.33-1",
     "santoku-system >= 0.0.56-1",
     "santoku-test-runner >= 0.0.23-1",
-    "santoku-make >= 0.0.127-1",
+    "santoku-make >= 0.0.128-1",
     "argparse >= 0.7.1-1",
   },
 
@@ -31,6 +31,5 @@ env.tarball = env.name .. "-" .. env.version .. ".tar.gz"
 env.download = env.homepage .. "/releases/download/" .. env.version .. "/" .. env.tarball
 
 return {
-  type = "lib",
   env = env
 }
