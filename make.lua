b0377e4951e6d153b4369d4bc6d479364ccc1984
broma0 <121064585+broma0@users.noreply.github.com>
2025-11-28 22:41:37 -0500

    bump version to 0.0.289-1

--- make.lua
+++ make.lua
@@ -1,7 +1,7 @@
 local env = {
 
   name = "santoku-cli",
-  version = "0.0.288-1",
+  version = "0.0.289-1",
   variable_prefix = "TK_CLI",
   license = "MIT",
   public = true,
@@ -14,7 +14,7 @@ local env = {
     "santoku-bundle >= 0.0.34-1",
     "santoku-system >= 0.0.56-1",
     "santoku-test-runner >= 0.0.23-1",
-    "santoku-make >= 0.0.136-1",
+    "santoku-make >= 0.0.137-1",
     "argparse >= 0.7.1-1",
   },
 
