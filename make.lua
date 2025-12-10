365999571fc3a58a004b8ad1405671f52764c210
Matthew Brooks <mb964033@gmail.com>
2025-12-10 18:31:58 -0500

    update

--- make.lua
+++ make.lua
@@ -1,18 +1,18 @@
 local env = {
   name = "santoku-cli",
-  version = "0.0.325-1",
+  version = "0.0.326-1",
   variable_prefix = "TK_CLI",
   license = "MIT",
   public = true,
   dependencies = {
     "lua >= 5.1",
-    "santoku >= 0.0.310-1",
+    "santoku >= 0.0.312-1",
     "santoku-fs >= 0.0.41-1",
     "santoku-template >= 0.0.34-1",
     "santoku-bundle >= 0.0.39-1",
     "santoku-system >= 0.0.61-1",
     "santoku-test-runner >= 0.0.25-1",
-    "santoku-make >= 0.0.179-1",
+    "santoku-make >= 0.0.180-1",
     "santoku-mustache >= 0.0.14-1",
     "argparse >= 0.7.1-1",
   },
@@ -22,6 +22,4 @@ env.homepage = "https://github.com/treadwelllane/lua-" .. env.name
 env.tarball = env.name .. "-" .. env.version .. ".tar.gz"
 env.download = env.homepage .. "/releases/download/" .. env.version .. "/" .. env.tarball
 
-return {
-  env = env
-}
+return { env = env }
