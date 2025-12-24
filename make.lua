534753bd734e4506ab7f5ffdd33acb15c65574d9
Matthew Brooks <mb964033@gmail.com>
2025-12-24 13:46:30 -0500

    update

--- make.lua
+++ make.lua
@@ -1,6 +1,6 @@
 local env = {
   name = "santoku-cli",
-  version = "0.0.332-1",
+  version = "0.0.333-1",
   variable_prefix = "TK_CLI",
   license = "MIT",
   public = true,
@@ -12,7 +12,7 @@ local env = {
     "santoku-bundle >= 0.0.39-1",
     "santoku-system >= 0.0.61-1",
     "santoku-test-runner >= 0.0.25-1",
-    "santoku-make >= 0.0.186-1",
+    "santoku-make >= 0.0.187-1",
     "santoku-mustache >= 0.0.14-1",
     "argparse >= 0.7.1-1",
   },
